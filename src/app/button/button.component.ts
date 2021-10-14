e42c0f646598d2729be1c3708d3f0a6f56f6fcf2
Jason <jmaalegado@gmail.com>
2021-10-14 22:55:45 +1100

    WIP - setting behavior of open and close buttons

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Entry } from '../records/entry/entry.model';
+import { AllEntriesService } from '../data/entries-data.service';
 
 @Component({
   selector: 'app-button',
@@ -8,9 +10,19 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() buttonName!: string;
 
-  constructor() { }
+  allEntries!: Entry[];
+
+  constructor(private allEntriesService: AllEntriesService) {}
 
   ngOnInit(): void {
+  }
+
+  public btnClickHandler(event: any):void {
+    const entries = this.allEntriesService.get();
+    const btnValue = event.target.textContent;
+    if (btnValue === 'Open All') {
+      entries.forEach(entry => entry.isEntryShown)
+    }
   }
 
 }
