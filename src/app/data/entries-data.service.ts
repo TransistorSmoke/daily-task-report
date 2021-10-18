2b053ee72430d7c40c296b499ec70fd82e6cbcf3
Jason <jmaalegado@gmail.com>
2021-10-18 21:24:04 +1100

    Changed color of row entry header; aded day to date pipe in the header

--- src/app/data/entries-data.service.ts
+++ src/app/data/entries-data.service.ts
@@ -7,6 +7,9 @@ import { Injectable } from "@angular/core";
 
 export class AllEntriesService {
     allEntries = [
+        new Entry (new Date('15 Oct 2021'), 'PDF generation with static data'),
+        new Entry (new Date('14 Oct 2021'), 'PDF generation method being cleaned up'),
+        new Entry (new Date('10 Oct 2021'), 'New age field component ready for release'),
         new Entry (new Date('25 Sept 2021'), 'Nothing new here, bro.'),
         new Entry (new Date('02 Sept 2021'), 'bugs found in new age field component'),
         new Entry (new Date('19 Sept 2021'), 'validation problems still with the component'),
