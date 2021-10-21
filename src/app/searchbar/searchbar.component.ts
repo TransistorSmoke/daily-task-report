b45775736dfcf4e32e943a39ce4427bc93e656f5
Jason <jmaalegado@gmail.com>
2021-10-21 22:39:09 +1100

    Set search input component as searchForm

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit  } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-searchbar',
-  templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.scss']
+	selector: 'app-searchbar',
+	templateUrl: './searchbar.component.html',
+	styleUrls: ['./searchbar.component.scss']
 })
-export class SearchbarComponent implements OnInit {
+export class SearchbarComponent implements OnInit, OnDestroy{
+	searchForm!: FormGroup;
+	destroy$: Subject<boolean> = new Subject<boolean>();
+	
+	searchInputControl!: AbstractControl;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.searchForm = new FormGroup({
+			"search": new FormControl(null)
+		});
+
+		this.searchInputControl = this.searchForm.controls['search'];
+		this.searchInputControl.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(() => {
+			
+		})
+		
+
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next(true);
+		this.destroy$.unsubscribe();
+	}
 
 }
