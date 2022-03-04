bf62547af3f2078c5f55a64b8b5976bca17c9060
Jason <jmaalegado@gmail.com>
2022-03-04 22:09:24 +1100

    updated package

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,39 +3,38 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Entry } from '../records/entry/entry.model';
 
 @Component({
-	selector: 'app-form',
-	templateUrl: './form.component.html',
-	styleUrls: ['./form.component.scss']
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-	@Input() arrayEntries!: Entry[];
-	@Output() onEntryEmit: EventEmitter<Object>;
+  @Input() arrayEntries!: Entry[];
+  @Output() onEntryEmit: EventEmitter<Object>;
 
-	currentDate: string;
-	recordsForm!: FormGroup;
-	newEntry!: Entry;
+  currentDate: string;
+  recordsForm!: FormGroup;
+  newEntry!: Entry;
 
-	constructor() { 
-		this.currentDate = Date();
-		this.onEntryEmit = new EventEmitter();
-	}
+  constructor() {
+    this.currentDate = Date();
+    this.onEntryEmit = new EventEmitter();
+  }
 
-	ngOnInit(): void {
-		this.recordsForm = new FormGroup({
-			"entry": new FormControl(null, Validators.required),
-		});
-	}
+  ngOnInit(): void {
+    this.recordsForm = new FormGroup({
+      entry: new FormControl(null, Validators.required),
+    });
+  }
 
+  /*
+   *	Emit the records form data from this current FormComponent (child) to the RecordsGroupComponent (parent).
+   *  Resets the form after emitting the data.
+   *
+   */
+  public entrySubmitHandler(): void {
+    this.onEntryEmit.emit(this.recordsForm);
 
-	/*
-	 *	Emit the records form data from this current FormComponent (child) to the RecordsGroupComponent (parent).
-	 *  Resets the form after emitting the data.
-	 *
-	*/
-	public entrySubmitHandler(): void {
-		this.onEntryEmit.emit(this.recordsForm);
-
-		// Reset the textfield input
-		this.recordsForm.setValue({"entry": ''});
-	}
+    // Reset the textfield input
+    this.recordsForm.setValue({ entry: '' });
+  }
 }
